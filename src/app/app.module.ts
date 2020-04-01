669de1acb6cc33169ba92e6df28a51dcec01103e
fattym <jnganga288@gmail.com>
2020-04-01 21:17:22 +0300

    changed files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
+import { QuoteComponent } from './quote/quote.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormComponent
+    FormComponent,
+    QuoteComponent
   ],
   imports: [
     BrowserModule,
