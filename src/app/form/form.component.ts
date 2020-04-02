6d1e75799bc5b07d4c51e5bf27212458b6a8b86c
fattym <jnganga288@gmail.com>
2020-04-02 10:45:33 +0300

    added form sceleton

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,3 +1,4 @@
+import { Quote } from './../quote';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
-
+newQuote =new Quote(0,"","", new Date());
   constructor() { }
 
   ngOnInit(): void {
